ee108c69d9383916697d7e74cccec500579c0cbb
Raghav-012 <your-github-email@example.com>
2025-11-09 23:31:54 +0530

    updated

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const API_END_POINT = "http://localhost:8080/api/v1/user";
+export const API_END_POINT =
+  process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1/user";
 
 export const options = {
     method: 'GET',
